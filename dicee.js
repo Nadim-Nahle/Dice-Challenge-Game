7f3238e6bb786f195b670c122c7361b527d1240e
Nadim <nahlenadim@gmail.com>
2022-05-23 10:22:05 +0300

    Creating the variables to get the dice pictures from dom

--- dicee.js
+++ dicee.js
@@ -1,10 +1,23 @@
 var random1;
 var random2;
 
+var dice1 = document.getElementById("dice1")
+var dice2 = document.getElementById("dice1")
+var dice3 = document.getElementById("dice1")
+var dice4 = document.getElementById("dice1")
+var dice5 = document.getElementById("dice1")
+var dice6 = document.getElementById("dice1")
+
+var dice1V2 = document.getElementById("dice1")
+var dice2V2 = document.getElementById("dice1")
+var dice3V2 = document.getElementById("dice1")
+var dice4V2 = document.getElementById("dice1")
+var dice5V2 = document.getElementById("dice1")
+var dice6V2 = document.getElementById("dice1")
+
 function RollDice(){
     random1 = Math.floor(Math.random() * 6)
     random2 = Math.floor(Math.random() * 6)
 }
-
 
 

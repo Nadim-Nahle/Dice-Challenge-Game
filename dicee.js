24bc44a3622e97b51da32a7eb2de97ef85d4f026
Nadim <nahlenadim@gmail.com>
2022-05-23 11:01:15 +0300

    Creating 2 function to roll result

--- dicee.js
+++ dicee.js
@@ -2,22 +2,65 @@ var random1;
 var random2;
 
 var dice1 = document.getElementById("img1")
-var dice2 = document.getElementById("dice1")
-var dice3 = document.getElementById("dice1")
-var dice4 = document.getElementById("dice1")
-var dice5 = document.getElementById("dice1")
-var dice6 = document.getElementById("dice1")
+var dice2 = document.getElementById("img2")
+var dice3 = document.getElementById("img3")
+var dice4 = document.getElementById("img4")
+var dice5 = document.getElementById("img5")
+var dice6 = document.getElementById("img6")
 
-var dice1V2 = document.getElementById("dice1")
-var dice2V2 = document.getElementById("dice1")
-var dice3V2 = document.getElementById("dice1")
-var dice4V2 = document.getElementById("dice1")
-var dice5V2 = document.getElementById("dice1")
-var dice6V2 = document.getElementById("dice1")
+var dice1V2 = document.getElementById("img1-1")
+var dice2V2 = document.getElementById("img2-1")
+var dice3V2 = document.getElementById("img3-1")
+var dice4V2 = document.getElementById("img4-1")
+var dice5V2 = document.getElementById("img5-1")
+var dice6V2 = document.getElementById("img6-1")
 
-function RollDice(){
-    random1 = Math.floor(Math.random() * 6)
-    random2 = Math.floor(Math.random() * 6)
+function rollDice(){
+    random1 = Math.ceil(Math.random() * 6);
+    random2 = Math.floor(Math.random() * 6);
+    result1();
+    result2();
 }
 
-dice1.style.width = "150px";
+function result1(){
+    if (random1 == 1){
+        dice1.style.width="160px";
+    }
+    if (random1 == 2){
+        dice2.style.width="160px";
+    }
+    if (random1 == 3){
+        dice3.style.width="160px";
+    }
+    if (random1 == 4){
+        dice4.style.width="160px";
+    }
+    if (random1 == 5){
+        dice5.style.width="160px";
+    }
+    if (random1 == 6){
+        dice6.style.width="160px";
+    }
+}
+function result2(){
+    if (random2 == 1){
+        dice1V2.style.width="160px";
+    }
+    if (random2 == 2){
+        dice2V2.style.width="160px";
+    }
+    if (random2 == 3){
+        dice3V2.style.width="160px";
+    }
+    if (random2 == 4){
+        dice4V2.style.width="160px";
+    }
+    if (random2 == 5){
+        dice5V2.style.width="160px";
+    }
+    if (random2 == 6){
+        dice6V2.style.width="160px";
+    }
+}
+
+rollDice();
